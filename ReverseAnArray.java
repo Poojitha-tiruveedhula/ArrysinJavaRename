bc52973efd833c4ca7d84d568bf564baba1a9be1
Poojitha-tiruveedhula <poojithatiruveedhula@gmail.com>
2024-12-07 17:43:54 +0530

    Reversing Array elements using Java

---
+++ ReverseAnArray.java
@@ -0,0 +1,21 @@
+package com.helloworld;
+
+public class Heyy
+{
+public static void main(String[] args)
+{
+int a[] ={1,2,3};
+System.out.print("normal is: ");
+for(int i=0;i<=a.length;i++)
+{
+System.out.print(a[i]+ " " );
+}
+System.out.println();
+System.out.print("reverse is: ");
+for(int i=a.length-1;i>=0;i--)
+{
+System.out.print(a[i]+ " ");
+}
+}}
+
+
