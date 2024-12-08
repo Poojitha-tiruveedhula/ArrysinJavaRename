a000c4c566e724de6a81a8590d16ff6b90dd5748
Poojitha-tiruveedhula <poojithatiruveedhula@gmail.com>
2024-12-08 15:31:14 +0530

    finding maximun and minimum element in java

---
+++ Maxmin.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Maxmin
+{
+public static void main(String[] args)
+{
+int a[] ={1,4,3,4,6};
+Arrays.sort(a);
+System.out.println("min:"+ a[0] + "max is:"+ a[a.length-1]);
+}
+}
