9228aad4f21115f2bf9774c0118ac4017292b3fe
Poojitha-tiruveedhula <poojithatiruveedhula@gmail.com>
2024-12-08 14:57:19 +0530

    Reverse a string in java

---
+++ Reversestring.java
@@ -0,0 +1,11 @@
+class Reversestring
+{
+public static void main(String[] args)
+{
+String original= "college";
+String temp = " ";
+for(int i=original.length()-1;i>=0;i--)
+temp+=original.charAt(i);
+System.out.println(temp);
+}
+}
