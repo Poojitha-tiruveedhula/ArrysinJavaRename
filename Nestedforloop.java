eb4587e770751c10776678928a6327d99dcd46b0
Poojitha-tiruveedhula <poojithatiruveedhula@gmail.com>
2024-12-17 19:16:19 +0530

    nested for loop in java

---
+++ Nestedforloop.java
@@ -0,0 +1,12 @@
+class Nestedforloop
+{
+public static void main(String[] args)
+{
+for(i=1;i<=5;i++)
+{
+System.out.println("day" + i);
+for(int j=1;j<=8;j++){
+System.out.println("  " + (j+1));
+}
+}
+}}
