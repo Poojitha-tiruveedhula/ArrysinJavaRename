f976ac38a5eb19270261744a6325aaab0f85859e
Poojitha-tiruveedhula <poojithatiruveedhula@gmail.com>
2024-12-18 13:52:39 +0530

    String in java

---
+++ Stringcode.java
@@ -0,0 +1,12 @@
+class Stringcode
+{
+public static void main(String[] args)
+{
+String name=new String("poojitha");
+System.out.println(name);
+System.out.println(name.hashCode());
+System.out.println("hi"+name);
+System.out.println(name.charAt(3));
+System.out.println(name.concat("hello"));
+}
+}
