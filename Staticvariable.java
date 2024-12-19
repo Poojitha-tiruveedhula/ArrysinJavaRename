eae68746dd545fe7a31f0ff130645ccdc405b654
Poojitha-tiruveedhula <poojithatiruveedhula@gmail.com>
2024-12-19 17:29:10 +0530

    static variabe usage in java

---
+++ Staticvariable.java
@@ -0,0 +1,36 @@
+class Hi
+{
+String company;
+int cost;
+static String name;
+public void display()
+{
+System.out.println(company+":"+cost+":"+name);
+}}
+
+public class Staticvariable
+{
+public static void main(String[] args)
+{
+Hi obj1=new Hi();
+obj1.company="redmi";
+obj1.cost=20000;
+Hi.name="phone";
+
+Hi obj2=new Hi();
+obj2.company="realme";
+obj2.cost=22000;
+Hi.name="phone";
+
+Hi obj3=new Hi();
+obj3.company="poco";
+obj3.cost=18000;
+Hi.name="phone";
+obj2.name="cell";
+
+obj1.display();
+obj2.display();
+obj3.display();
+}
+}
+
