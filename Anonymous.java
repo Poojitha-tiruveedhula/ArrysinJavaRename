38dd5e9ad9eb6f6d3848b65c054d11ea0127a0b3
Poojitha-tiruveedhula <poojithatiruveedhula@gmail.com>
2024-12-22 09:56:43 +0530

    Anonymous object creation in java

---
+++ Anonymous.java
@@ -0,0 +1,18 @@
+class Hi
+{
+public Hi()
+{
+System.out.println("object created");
+}
+public void display()
+{
+System.out.println("objects");
+}
+}
+class Anonymous
+{
+public static void  main(String[] args)
+{
+new Hi();
+}
+}
