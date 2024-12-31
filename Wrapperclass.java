49d5a1fcf1eb6ed0fe3460bcc0fde065c043dfae
Poojitha-tiruveedhula <poojithatiruveedhula@gmail.com>
2024-12-31 11:12:38 +0530

    wrapper class in java

---
+++ Wrapperclass.java
@@ -0,0 +1,16 @@
+class Wrapperclass
+{
+public static void main(String[] args)
+{
+int a=10;
+//Integer b=new Integer(a);
+Integer b=a ; 
+//int c=b.intValue();
+int c=b; 
+System.out.println(b);
+System.out.println(c);
+String string="20";
+int d=Integer.parseInt(string);
+System.out.println(d*2);
+}
+}
