a1be31c5bc23d16e267ba581241fcfcf32179b15
Poojitha-tiruveedhula <poojithatiruveedhula@gmail.com>
2025-01-26 19:02:48 +0530

    Race condition in java

---
+++ RaceCondition.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+class Counter
+{
+int count;
+public synchronized void increment()
+{
+count++;
+}
+}
+public class RaceCondition
+{
+public static void main(String[] args) throws InterruptedException
+{
+Counter a=new Counter();
+Runnable obj1=() ->
+{
+for(int i=1;i<=4;i++)
+{
+a.increment();
+
+}};
+Runnable obj2=()->
+{
+for(int i=1;i<=4;i++)
+{
+a.increment();
+}
+};
+Thread t1=new Thread(obj1);
+Thread t2=new Thread(obj2);
+t1.start();
+t2.start();
+t1.join();
+t2.join();
+System.out.println(a.count);
+}
+}
+
