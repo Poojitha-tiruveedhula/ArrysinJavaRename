0d8ad1225d293c58537fa3cb1a41bfc55f16e2a1
Poojitha-tiruveedhula <poojithatiruveedhula@gmail.com>
2025-01-27 09:50:40 +0530

    A program using methods in java

---
+++ UsingMethods.java
@@ -0,0 +1,26 @@
+class UsingMethods
+{
+public int add(int n1,int n2,int n3)
+{
+ return n1+n2+n3;
+}
+
+public int add(int n1,int n2)
+{
+return n1+n2;
+}
+public double add(double n1,int n2)
+{
+return n1+n2;
+}
+}
+public class America
+{
+public static void main(String[] args)
+{
+Austria obj=new Austria();
+double n=obj.add(2,3);
+System.out.println(n);
+}
+}
+
