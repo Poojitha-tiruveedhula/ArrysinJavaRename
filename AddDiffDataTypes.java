b86239e11e170583f2db9be084cd0dff18454a25
Poojitha-tiruveedhula <poojithatiruveedhula@gmail.com>
2025-01-27 09:48:08 +0530

    Adding different data types in java

---
+++ AddDiffDataTypes.java
@@ -0,0 +1,11 @@
+public class AddDiffDataTypes
+{  
+public static void main(String[] args) 
+{  
+int num1 = 5;  
+double num2 = 2.5;  
+char character = 'A';  
+double result = num1 + num2 + character;  
+System.out.println(result);  
+}  
+}
