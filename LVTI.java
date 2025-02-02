a37d0d756b03e13bc036c684c740d1d39b07c27f
Poojitha-tiruveedhula <poojithatiruveedhula@gmail.com>
2025-02-02 18:50:22 +0530

    Local variable type interface usage in java

---
+++ LVTI.java
@@ -0,0 +1,17 @@
+public class LVTI
+{
+public static void main(String[] args)
+{
+var a=10;
+int d=30;
+String var="pooja";
+int c[]=new int[30];
+var e=new int[6];
+System.out.println(a);
+System.out.println(var);
+
+System.out.println(e);
+
+
+}
+}
