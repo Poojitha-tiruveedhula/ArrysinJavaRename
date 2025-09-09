b28d6769b6085b13842d643d3434d3bbfcf66311
Poojitha-tiruveedhula <poojithatiruveedhula@gmail.com>
2025-09-09 19:09:48 +0530

    Replace string with substring
    Replace string with substring

---
+++ ReplaceStringWithSubstring.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+public class ReplaceStringWithSubstring
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+String str=sc.nextLine();
+String str1=sc.nextLine();
+String str2=sc.nextLine();
+String res=str.replace(str1,str2);
+System.out.println(res);
+}}
+   
