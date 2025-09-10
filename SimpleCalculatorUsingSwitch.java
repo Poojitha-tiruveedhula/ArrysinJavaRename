ea78dcdeeb269af0527e260dac68502aa3b9548b
Poojitha-tiruveedhula <poojithatiruveedhula@gmail.com>
2025-09-10 08:23:59 +0530

    Simple Calculator Using Switch
    Simple Calculator Using Switch

---
+++ SimpleCalculatorUsingSwitch.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.Scanner;
+public class SimpleCalculatorUsingSwitch
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+char c=sc.next().charAt(0);
+switch(c)
+{
+case'+':
+System.out.println(a+b);
+break;
+case'-':
+System.out.println(a-b);
+break;
+case'*':
+System.out.println(a*b);
+break;
+case'/':
+System.out.println(a/b);
+break;
+default:
+System.out.println("invalid operation");
+}
+}
+}
